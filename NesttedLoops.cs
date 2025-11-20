664a99cbaee06c7fbec282f99287cf00c9b30613
Liam Mazuz <lucariops946@gmail.com>
2025-11-20 13:27:22 +0200

    Add project files.

---
+++ NesttedLoops.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9thGrade
+{
+    internal class NesttedLoops
+    {
+        public static void UnitTest()
+        {
+            Targil1_d();
+        }
+
+        public static void Targil1_a() // יוצר מלבן
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void Targil1_b() // יוצר משולש
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+
+        }
+
+        public static void Targil1_c() // יוצר ריבוע של 1-5
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                    Console.Write(j);
+                Console.WriteLine();
+            }
+        }
+
+        public static void Targil1_d() // משולש הפוך
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 5; j >= i; j--)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+
+        public static void Targil2() //
+        {
+
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _9thGrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NesttedLoops.UnitTest();
+        }
+    }
+}
