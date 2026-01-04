79d5abaa1d41bf7539d04506e80c158223a5db0e
Liam Mazuz <lucariops946@gmail.com>
2026-01-04 11:43:20 +0200

    ARRAYS

---
+++ ArraysPractice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9thGrade
+{
+    internal class ArraysPractice
+    {
+        public static void UnitTest()
+        {
+           T10_6();
+        }
+        public static void T10_6()
+        {
+            double[] grades=new double[10];
+            double avg, sum=0,dist;
+            for(int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine("Enter your grade:");
+                grades[i]=double.Parse(Console.ReadLine());
+                sum=sum+grades[i];
+            }
+            avg =(sum / grades.Length);
+            for (int i = 0; i < grades.Length; i++)
+            {
+                dist = Math.Abs(grades[i] - avg);
+                Console.WriteLine("The distance between grade " + grades[i] + " and the average " + avg + " is: " + dist);
+            }
+
+        }
+    }
+
+}
