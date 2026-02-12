8ffe00218dd211dc552b10f204d86a367bea9a54
Liam Mazuz <lucariops946@gmail.com>
2026-02-12 13:29:28 +0200

    New

--- Point.cs
+++ Point.cs
@@ -14,9 +14,9 @@ namespace _9thGrade
             Point p1 = new Point(3, 4);
             Console.WriteLine(p1);
             p1.Getx(5.5);
-            Console.WriteLine(p1.SetX(5));
+            Console.WriteLine(p1.SetX());
             p1.Gety(6);
-            Console.WriteLine(p1.Sety(6));
+            Console.WriteLine(p1.Sety());
             Console.WriteLine(p1);
         }
 
@@ -34,7 +34,7 @@ namespace _9thGrade
              this.x=x;
         }
 
-        public double SetX(double x)
+        public double SetX()
         {
             return this.x;
         }
@@ -44,7 +44,7 @@ namespace _9thGrade
              this.y = y;
         }
 
-        public double Sety(double y)
+        public double Sety()
         {
             return this.y;
         }
